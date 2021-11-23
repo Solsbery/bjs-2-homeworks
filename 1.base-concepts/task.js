23c6b9b3a19a7da7eb4e05df5152c5e1ac05f42e
Nadezhda Shulga <por.que@yandex.ru>
2021-11-23 22:09:18 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,45 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  return arr; // array
+  let x;
+    let D = b ** 2 - 4 * a * c;
+
+    if (D < 0) {
+      return [];
+    } else if (D === 0) {
+      return [-b / 2 * a];
+    } else {
+      return [(-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a)];
+    }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    return `Параметр <процентная ставка> содержит неправильное значение ${percent}`;
+  }
+  const parsedPercent = parseFloat(percent);
 
-  return totalAmount;
+  if (isNaN(contribution)) {
+    return `Параметр <сумма первоначального взноса> содержит неправильное значение ${contribution}`;
+  }
+  const parsedContribution = parseFloat(contribution);
+
+  if (isNaN(amount)) {
+    return `Параметр <сумма кредита> содержит неправильное значение ${amount}`;
+  }
+  const parsedAmount = parseFloat(amount);
+
+  let creditAmount = parsedAmount - parsedContribution;
+  let term = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+  let monthPercent = parsedPercent / 100 / 12;
+  let monthPayment = creditAmount * (monthPercent + monthPercent / (((1 + monthPercent) ** term) - 1));
+  totalAmount = monthPayment * term;
+  console.log(totalAmount.toFixed(2));
+
+  return parseFloat(totalAmount.toFixed(2))
 }
